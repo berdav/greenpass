2cbcaf08eb7cd4d23dd91e7c73dd4bd4f79a397e
Davide Berardi <berardi.dav@gmail.com>
2021-10-30 18:38:39 +0200

    Add tzlocal as a dependency

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.md") as f:
 setup(
     name             = 'greenpass',
     scripts          = [ maintool_dst_path ],
-    version          = '3.7',
+    version          = '3.8',
     license          = 'LGPLv3',
     description      = 'Scriptable green pass verifier',
     long_description = description,
@@ -45,7 +45,7 @@ setup(
     author           = 'Davide Berardi',
     author_email     = 'berardi.dav@gmail.com',
     url              = 'https://github.com/berdav/greenpass',
-    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v3.7.zip',
+    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v3.8.zip',
     keywords         = [ 'covid19', 'greenpass', 'certificates', 'authorization' ],
     install_requires = [
         'base45',
@@ -60,6 +60,7 @@ setup(
         'pyzbar',
         'requests',
         'termcolor',
+        'tzlocal',
         'cose'
     ],
     classifiers      = [
