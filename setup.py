a814918e18b5380a303cc901d8bda9ea41203e38
Davide Berardi <berardi.dav@gmail.com>
2021-11-19 12:27:15 +0100

    Version 4.0 automated Pipy push
    Now the push on Pypi is automated, because I've made some test I need a
    new version number.

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@
 import os
 import shutil
 from distutils.core import setup
+import setuptools
 
 maintool = "greenpass"
 
@@ -35,7 +36,7 @@ with open("README.md") as f:
 setup(
     name             = 'greenpass',
     scripts          = [ maintool_dst_path ],
-    version          = '3.9',
+    version          = '4.0',
     license          = 'LGPLv3',
     description      = 'Scriptable green pass verifier',
     long_description = description,
@@ -44,7 +45,7 @@ setup(
     author           = 'Davide Berardi',
     author_email     = 'berardi.dav@gmail.com',
     url              = 'https://github.com/berdav/greenpass',
-    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v3.9.zip',
+    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v4.0.zip',
     keywords         = [ 'covid19', 'greenpass', 'certificates', 'authorization' ],
     install_requires = [
         'base45',
