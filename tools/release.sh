e1eb3f8336ce1e4c131d79b148ceba0a49a1d2b3
Davide Berardi <berardi.dav@gmail.com>
2021-11-19 15:32:17 +0100

    Release script to help the new tag procedure
    The tag procedure is error prone, better automatize it as the rest of
    the CI.

---
+++ tools/release.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+NEXT_VER="${1:-newversion}"
+
+get_current_version() {
+  git tag --list | sort -V | tail -1 | sed 's/^v//g'
+}
+
+increment() {
+  echo "$1" | awk -F. -v OFS=. '{$NF++;print}'
+}
+
+release() {
+  sed -i \
+    "s#\(version[\t ]*=[\t ]*'\)[0-9]\+\.[0-9]\+\.[0-9]\+#\1$1#" \
+    setup.py
+  sed -i \
+    "s#\(download_url[\t ].*/v\)[0-9]\+\.[0-9]\+\.[0-9]\+\.zip'#\1$1.zip'#" \
+    setup.py
+}
+
+if test "x$NEXT_VER" = "xnewversion"; then
+  NEXT_VER="$(increment $(get_current_version))"
+fi
+
+release "$NEXT_VER"
+git add setup.py
+git commit -m "$NEXT_VER release"
+git tag "$NEXT_VER"
