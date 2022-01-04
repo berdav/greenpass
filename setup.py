1ba2f2ef80fe4f14c60e6bd2c5b89009650bf836
Davide Berardi <berardi.dav@gmail.com>
2022-01-04 11:56:02 +0100

    v4.2.4 release

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open("README.md") as f:
 setup(
     name             = 'greenpass',
     scripts          = [ maintool_dst_path ],
-    version          = '4.2.3',
+    version          = '4.2.4',
     license          = 'LGPLv3',
     description      = 'Scriptable green pass verifier',
     long_description = description,
@@ -48,7 +48,7 @@ setup(
     author           = 'Davide Berardi',
     author_email     = 'berardi.dav@gmail.com',
     url              = 'https://github.com/berdav/greenpass',
-    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v4.2.3.zip',
+    download_url     = 'https://github.com/berdav/greenpass/archive/refs/tags/v4.2.4.zip',
     keywords         = [ 'covid19', 'greenpass', 'certificates', 'authorization' ],
     install_requires = [
         'base45',
